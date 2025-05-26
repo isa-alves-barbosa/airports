574fa45cf6b6c30913e09ad172d8ab8ea68cc45a
isabela <isabela@gmail.com>
2025-05-26 14:37:21 -0300

    Término da Fase 3

--- src/main/java/local/isabela/airports/controllers/AirportController.java
+++ src/main/java/local/isabela/airports/controllers/AirportController.java
@@ -1,12 +1,14 @@
-
 package local.isabela.airports.controllers;
 
 import java.util.List;
 import local.isabela.airports.entities.Airport;
 import local.isabela.airports.service.AirportService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
 
 
 /**
@@ -25,8 +27,32 @@ public class AirportController {
     * @return
     */
    @GetMapping("/airport")
-   public List<Airport>findAll(){
+   public List<Airport>fidAll(){
        List<Airport> result = airportService.findAll();
        return result;
    }
+
+/**
+ * Endpoint /airports/city/{cityName}
+ * preparando para devolver código de status conforme 
+ * padronização REST.
+ * @param cityName 
+ * @return 
+ */
+@GetMapping ("/city/{cityName}")
+public ResponseEntity<List<Airport>> findByCityIgnoreCase(@PathVariable String cityName){
+ List<Airport> result = airportService.findByCity(cityName);
+
+if (result.isEmpty()) {
+ //Ops..lista vazia...
+ //notFound devolve 404
+ return ResponseEntity.notFound().build();
+}else {
+ //Eba! Tem dados!
+ //ok devolve 200
+ return ResponseEntity.ok(result);
+
+}
+
+}
 }
--- src/main/java/local/isabela/airports/repositories/AirportRepository.java
+++ src/main/java/local/isabela/airports/repositories/AirportRepository.java
@@ -1,6 +1,7 @@
 package local.isabela.airports.repositories;
 
 
+import java.util.List;
 import local.isabela.airports.entities.Airport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,5 +10,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author ppjata
  */
 public interface AirportRepository extends JpaRepository<Airport, Long>{
-    
+    List<Airport> findByCityIgnoreCase(String city);
 }
